d0bd0a8c16e2b0d0cf85d51ac306fdbddd612122
User <User@S18>
2018-10-02 13:36:14 +0200

    succefully updated

--- src/za/co/reverside/training/p03/Star.java
+++ src/za/co/reverside/training/p03/Star.java
@@ -1,399 +1,346 @@
-
 package za.co.reverside.training.p03;
 
-
 public class Star {
-   
-public void squareStar(int row){
-	
+
+    public void squareStar(int row) {
+
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j <row; j++) {
+            for (int j = 0; j < row; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-}
-	
+    }
 
-public void hollowSquare(int row){
+    public void hollowSquare(int row) {
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j <row; j++) {
-                if(i==0 || i==4){
-                System.out.print("*");
-            }else if(j==0 || j==4){
-                System.out.print("*"+"   ");
-                        }else{
-                  System.out.print("");
-            }
+            for (int j = 0; j < row; j++) {
+                if (i == 0 || i == 4) {
+                    System.out.print("*");
+                } else if (j == 0 || j == 4) {
+                    System.out.print("*" + "   ");
+                } else {
+                    System.out.print("");
+                }
             }
             System.out.println();
         }
-}	
+    }
 
-public void hollowSquareStar(int row){
+    public void hollowSquareStar(int row) {
 
-        for(int i=1;i<=row;i++)
-     {
-         for(int j=1;j<=row;j++)
-         {
-             if(i==j ||i==1 || i ==row || j==1 || j==row||i*2==j||i/2==j){
-              System.out.print("*");
-             }else{
-              System.out.print(" ");
-             }
-             
-         }
-         System.out.println("");
-     }
-}
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= row; j++) {
+                if (i == j || i == 1 || i == row || j == 1 || j == row || i * 2 == j || i / 2 == j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
 
-public void rhombusStar(int row){
+            }
+            System.out.println("");
+        }
+    }
 
-        for(int i=1;i<=row;i++)
-     {
-         for(int j=i;j<row;j++)
-         {
-             System.out.print(" ");
-         }
-         for(int j=1;j<=row;j++)
-         {
-             System.out.print("*");
-         }
-         System.out.println("");
-     }
-} 
+    public void rhombusStar(int row) {
 
-public void hollowRhombus(int row){
+        for (int i = 1; i <= row; i++) {
+            for (int j = i; j < row; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= row; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 
-     
-        for(int i=1;i<=row;i++)
-     {
-         for(int j=i;j<row;j++)
-         {
-             System.out.print(" ");
-         }
-         for(int j=1;j<=row;j++)
-         {
-             if(i==1 || i ==row || j==1 || j==row){
-              System.out.print("*");
-             }else{
-              System.out.print(" ");}
+    public void hollowRhombus(int row) {
 
-         }
-         System.out.println("");
-     }
-} 
+        for (int i = 1; i <= row; i++) {
+            for (int j = i; j < row; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= row; j++) {
+                if (i == 1 || i == row || j == 1 || j == row) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
 
-public void mirroredRhombus(int row){
+            }
+            System.out.println("");
+        }
+    }
 
-     int num2=0;
-        for(int i=1;i<=row;i++)
-     {
-         for(int j=0;j<num2;j++)
-         {
-             System.out.print(" ");
-         }
-         num2++;
-         for(int j=1;j<=row;j++)
-         {
-             
-              System.out.print("*");
-            
+    public void mirroredRhombus(int row) {
 
-         }
-         System.out.println("");
-     }
-}
+        int num2 = 0;
+        for (int i = 1; i <= row; i++) {
+            for (int j = 0; j < num2; j++) {
+                System.out.print(" ");
+            }
+            num2++;
+            for (int j = 1; j <= row; j++) {
 
-public void hollowMirroredRhombus(int row){
+                System.out.print("*");
 
-     int num2=0;
-        for(int i=1;i<=row;i++)
-     {
-         for(int j=0;j<num2;j++)
-         {
-             System.out.print(" ");
-         }
-         num2++;
-         for(int j=1;j<=row;j++)
-         {
-             if(i==1 || i ==row || j==1 || j==row){
-              System.out.print("*");
-             }else{
-              System.out.print(" ");}
+            }
+            System.out.println("");
+        }
+    }
 
-         }
-         System.out.println("");
-     }
-} 
-//RIGHT TRIANGLE STAR PATTERN
-static void rightTriangle(int row){
+    public void hollowMirroredRhombus(int row) {
 
-        for(int i=1;i<=row;i++)
-     {
-        for(int j=1;j<=i;j++)
-         {
-             System.out.print("*");
-         }
-         System.out.println("");
-	 }
-}
-//HOLLOW RIGHT TRIANGLE STAR PATTERN
-static void hollowRightTriangle(int row){
+        int num2 = 0;
+        for (int i = 1; i <= row; i++) {
+            for (int j = 0; j < num2; j++) {
+                System.out.print(" ");
+            }
+            num2++;
+            for (int j = 1; j <= row; j++) {
+                if (i == 1 || i == row || j == 1 || j == row) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
 
-        for(int i=1;i<=row;i++)
-     {
-         for(int j=1;j<=i;j++)
-         {
-            if(i==row||j==1||j==i){
-             System.out.print("*");
-             }else{
-             System.out.print(" ");
-             }
-             
-         }
-         System.out.println("");
-	 }
-//MIRRORED RIGHT TRIANGLE STAR PATTERN
-static void mirroredRightTriangle(int row){
+            }
+            System.out.println("");
+        }
+    }
 
-        for(int i=1;i<=row;i++)
-     {
-         for(int j=i;j<row;j++)
-         {
-             System.out.print(" ");
-         }
-         for(int j=1;j<=i;j++)
-         {
-             System.out.print("*");
-         }
-         System.out.println("");
-     }
- }
-//HOLLOW MIRRORED RIGHT TRIANGLE STAR PATTERN
-static void hollowMirrpredRightTriangle(int row){
+    public void rightTriangle(int row) {
 
-        for(int i=1;i<=row;i++)
-     {
-         for(int j=i;j<row;j++)
-         {
-             System.out.print(" ");
-         }
-         for(int j=1;j<=i;j++)
-         {
-            if(i==row||j==1||j==i){
-             System.out.print("*");
-             }else{
-             System.out.print(" ");
-             }
-         }
-         System.out.println("");
-	 }
-}
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    public void hollowRightTriangle(int row) {
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i == row || j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println("");
+        }
+    }
+
+    public void mirroredRightTriangles(int row) {
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = i; j < row; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        
+    }
+
+    public void hollowMirrpredRightTriangle(int row) {
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = i; j < row; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (i == row || j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
 //INVERTED RIGHT TRIANGLE STAR PATTERN
-static void invertedRightTriangle(int row){
 
-        for(int i=row;i>=1;i--)
-     {
-         
-         for(int j=1;j<=i;j++)
-         {
-           
-             System.out.print("*");
-            
-         }
-         System.out.println("");
-	 }
-}
-		 
-//HOLLOW INVERTED RIGHT TRIANGLE STAR PATTERN
-static void hollowInvertedRightTraingle(int row){
+    public void invertedRightTriangle(int row) {
 
-        for(int i=row;i>=1;i--)
-     {
-         
-         for(int j=1;j<=i;j++)
-         {
-           if(i==row||j==1||j==i){
-             System.out.print("*");
-           }else{
-             System.out.print(" ");
-           }
-            
-         }
-         System.out.println("");
-	 }
-}
-//INVERTED MIRRORED RIGHT TRIANGLE STAR PATTERN
-static void invertedMirroredRightTriangle(int row){
+        for (int i = row; i >= 1; i--) {
 
-        for(int i=row;i>=1;i--)
-     {
-         for(int j=i;j<row;j++)
-         {
-             System.out.print(" ");
-         }
-         for(int j=1;j<=i;j++)
-         {
-           
-             System.out.print("*");
-            
-         }
-         System.out.println("");
-	 }
-}
+            for (int j = 1; j <= i; j++) {
 
-//HOLLOW INVERTED MIRRORED RIGHT TRIANGLE STAR PATTERN
-static void hollowInvertedMirroredRightTriangle(int row){
+                System.out.print("*");
 
-        for(int i=row;i>=1;i--)
-     {
-         for(int j=i;j<row;j++)
-         {
-             System.out.print(" ");
-         }
-         for(int j=1;j<=i;j++)
-         {
-            if(i==row||j==1||j==i){
-             System.out.print("*");
-             }else{
-             System.out.print(" ");
-             }
-         }
-         System.out.println("");
-	 }
-}	 
-//PYRAMID STAR PATTERN	
+            }
+            System.out.println("");
+        }
+    }
 
-	static void pyramid(int row){
 
-       int k = 2*row - 2; 
-   
-        for (int i=0; i<row; i++) 
-        { 
-            for (int j=0; j<k; j++) 
-            { 
-                System.out.print(" "); 
-            } 
-            k = k - 1; 
-            for (int j=0; j<=i; j++ ) 
-            { 
-                System.out.print("* "); 
-            } 
-            System.out.println(); 
-		}
-	}
-			
-//HOLLOW PYRAMID STAR PATTERN	
-static void hollowPyramid(int row){
-			
-        for (int i = 1; i <= row; i++)
-        {
-         
+    public void hollowInvertedRightTraingle(int row) {
+
+        for (int i = row; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+                if (i == row || j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println("");
+        }
+    }
+
+
+    public void invertedMirroredRightTriangle(int row) {
+
+        for (int i = row; i >= 1; i--) {
+            for (int j = i; j < row; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print("*");
+
+            }
+            System.out.println("");
+        }
+    }
+
+
+    public void hollowInvertedMirroredRightTriangle(int row) {
+
+        for (int i = row; i >= 1; i--) {
+            for (int j = i; j < row; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (i == row || j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+	
+
+    public void pyramid(int row) {
+
+        int k = 2 * row - 2;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < k; j++) {
+                System.out.print(" ");
+            }
+            k = k - 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+	
+    public void hollowPyramid(int row) {
+
+        for (int i = 1; i <= row; i++) {
+
             for (int j = i; j < row; j++) {
                 System.out.print(' ');
             }
-            for (int k = 1; k < 2*i; k++)
-            {
-             
-                if (i == row || (k == 1 || k == 2*i - 1)) {
+            for (int k = 1; k < 2 * i; k++) {
+
+                if (i == row || (k == 1 || k == 2 * i - 1)) {
                     System.out.print('*');
-                }
-                else {
+                } else {
                     System.out.print(' ');
                 }
             }
-            
-            System.out.println(); 
-}
-}
-			
-//INVERTED PYRAMID STAR PATTERN
-static void invertedPyramid(int row){
-        for (int i = row; i >= 1; i--)
-        {
+
+            System.out.println();
+        }
+    }
+
+
+    public void invertedPyramid(int row) {
+        for (int i = row; i >= 1; i--) {
             for (int j = row; j > i; j--) {
                 System.out.print(' ');
             }
 
-           
-            for (int k = 1; k < 2*i; k++) {
+            for (int k = 1; k < 2 * i; k++) {
                 System.out.print('*');
             }
-            System.out.println(); 
-		}
-}		
-//HOLLOW INVERTED PYRAMID STAR PATTERN
-static void hollowInvertedPyramid(int row){   
-       for (int i = row; i >= 1; i--)
-        {
+            System.out.println();
+        }
+    }
+
+
+    public void hollowInvertedPyramid(int row) {
+        for (int i = row; i >= 1; i--) {
             for (int j = row; j > i; j--) {
                 System.out.print(' ');
             }
-            for (int k = 1; k < 2*i; k++) {
-            
-                if (i == row || (k == 1 || k == 2*i - 1)) {
+            for (int k = 1; k < 2 * i; k++) {
+
+                if (i == row || (k == 1 || k == 2 * i - 1)) {
                     System.out.print('*');
-                }
-                else {
+                } else {
                     System.out.print(' ');
                 }
             }
 
-           
-            System.out.println(); 
-		}
-}
-			
-//HALF DIAMOND STAR PATTERN
-static void halfDiamondStar(int row){
+            System.out.println();
+        }
+    }
 
-     for(int i=1;i<=row;i++)
-     {
-         for(int j=1;j<=i;j++)
-         {
-             System.out.print("*");
-         }
-         System.out.println("");
-     } 
- for(int i=row;i>=1;i--)
-     {
-         for(int j=1;j<i;j++)
-         {
-             System.out.print("*");
-         }
-         System.out.println("");
-     } 
-          
-            System.out.println(); 
-}
+
+    public void halfDiamondStar(int row) {
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i = row; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        System.out.println();
+    }
+
 			
-//MIRRORED HALF DIAMOND STAR PATTERN			
-static void mirroredHalfDiamond(int row){
- for(int i=1;i<=row;i++)
-     {
-         for(int j=i;j<row;j++)
-         {
-             System.out.print(" ");
-         }
-         for(int j=1;j<=i;j++)
-         {
-             System.out.print("*");
-         }
-         System.out.println("");
-     } 
- for(int i=row;i>=1;i--)
-     {
-         for(int j=i;j<=row;j++)
-         {
-             System.out.print(" ");
-         }
-         for(int j=1;j<i;j++)
-         {
-             System.out.print("*");
-         }
-            
-            System.out.println(); 
-	 }
-}
+    public void mirroredHalfDiamond(int row) {
+        for (int i = 1; i <= row; i++) {
+            for (int j = i; j < row; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i = row; i >= 1; i--) {
+            for (int j = i; j <= row; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 }
--- src/za/co/reverside/training/p03/TestStar.java
+++ src/za/co/reverside/training/p03/TestStar.java
@@ -1,25 +1,51 @@
-
 package za.co.reverside.training.p03;
 
-
 public class TestStar {
-     public static void main(String[] args) {
-     Star star = new Star();
-     int row=5;
-     System.out.println("Exercise 1");
-     star.squareStar(row);
-      System.out.println("\n=======================\nExercise 2");
-     star.hollowSquare(row);
-      System.out.println("\n=======================\nExercise 3");
-      star.hollowSquareStar(row);
-      System.out.println("\n=======================\nExercise 4");
-      star.rhombusStar(row);
-      System.out.println("\n=======================\nExercise 5");
-      star.hollowRhombus(row);
-      System.out.println("\n=======================\nExercise 6");
-      star.mirroredRhombus(row);
-      System.out.println("\n=======================\nExercise 7");
-      star.hollowMirroredRhombus(row);
-      System.out.println("\n=======================\nExercise 8");
-     }
+    
+    public static void main(String[] args) {
+        Star star = new Star();
+        int row = 5;
+        System.out.println("Exercise 1");
+        star.squareStar(row);
+        System.out.println("\n=======================\nExercise 2");
+        star.hollowSquare(row);
+        System.out.println("\n=======================\nExercise 3");
+        star.hollowSquareStar(row);
+        System.out.println("\n=======================\nExercise 4");
+        star.rhombusStar(row);
+        System.out.println("\n=======================\nExercise 5");
+        star.hollowRhombus(row);
+        System.out.println("\n=======================\nExercise 6");
+        star.mirroredRhombus(row);
+        System.out.println("\n=======================\nExercise 7");
+        star.hollowMirroredRhombus(row);
+        System.out.println("\n=======================\nExercise 8");
+        star.rightTriangle(row);
+        System.out.println("\n=======================\nExercise 9");
+        star.hollowRightTriangle(row);
+        System.out.println("\n=======================\nExercise 10");
+        star.mirroredRightTriangles(row);
+        System.out.println("\n=======================\nExercise 11");
+        star.hollowMirrpredRightTriangle(row);
+        System.out.println("\n=======================\nExercise 12");
+        star.invertedRightTriangle(row);
+        System.out.println("\n=======================\nExercise 13");
+        star.hollowInvertedRightTraingle(row);
+        System.out.println("\n=======================\nExercise 14");
+        star.invertedMirroredRightTriangle(row);
+        System.out.println("\n=======================\nExercise 15");
+        star.hollowInvertedMirroredRightTriangle(row);
+        System.out.println("\n=======================\nExercise 16");
+        star.pyramid(row);
+        System.out.println("\n=======================\nExercise 17");
+        star.hollowPyramid(row);
+        System.out.println("\n=======================\nExercise 18");
+        star.invertedPyramid(row);
+        System.out.println("\n=======================\nExercise 19");
+        star.hollowInvertedPyramid(row);
+        System.out.println("\n=======================\nExercise 20");
+        star.halfDiamondStar(row);
+        System.out.println("\n=======================\nExercise 21");
+        star.mirroredHalfDiamond(row);
+    }
 }
